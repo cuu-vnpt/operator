0ee0e6255b48261dcecb23b6a6502f72faf5d812
cuu-vnpt <kunnobita97@gmail.com>
2023-12-13 23:49:21 +0700

    new file

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+float width;
+float height;
+
+Console.Write("Enter width: ");
+width = float.Parse(Console.ReadLine());
+Console.Write("Enter height: ");
+height = float.Parse(Console.ReadLine());
+
+float area = width * height;
+Console.WriteLine("Area is: " + area);
